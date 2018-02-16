3175ea844d3a6cfa40c2d29fe99fa3ca126d495a
Sowmya <Sow@10.0.0.6>
2018-02-15 21:36:16 -0500

    added new lines

--- first program/src/gitHubExample/Handshakeserverexample.java
+++ first program/src/gitHubExample/Handshakeserverexample.java
@@ -1,5 +1,13 @@
 package gitHubExample;
 
 public class Handshakeserverexample {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("commit and push for github");
+		
+	}
 
 }
+
+
