64a63599aac6a67fa472a44e25000d2f881de033
Sowmya <Sow@Sowmyas-MacBook-Pro.local>
2018-02-15 20:54:16 -0500

    Added first program
    Added first program

---
+++ first program/src/sowmya1.java
@@ -0,0 +1,16 @@
+
+public class sowmya1 {
+	
+	public static void main(String  args[]) {
+		
+				System.out.println("My first program");
+				System.out.println("My second program");
+				
+				
+				}
+				
+			
+	}
+	
+
+
