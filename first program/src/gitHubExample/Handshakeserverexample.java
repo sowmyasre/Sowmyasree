2a558b40c9639133a468f737829b7f8d73e228f6
Sowmya <Sow@10.0.0.6>
2018-02-15 21:40:47 -0500

    Revert "added new lines"
    This reverts commit 3175ea844d3a6cfa40c2d29fe99fa3ca126d495a.

--- first program/src/gitHubExample/Handshakeserverexample.java
+++ first program/src/gitHubExample/Handshakeserverexample.java
@@ -1,13 +1,5 @@
 package gitHubExample;
 
 public class Handshakeserverexample {
-	
-	public static void main(String[] args) {
-		
-		System.out.println("commit and push for github");
-		
-	}
 
 }
-
-
